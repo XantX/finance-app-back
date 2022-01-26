1c468579539c2e17dd316fbfe79975914412134a
XantX <hp_xantx@hotmail.com>
2022-01-26 13:30:34 -0500

    agregados metodos crud  para el banco

--- src/main/java/com/xantx/financeapp/controllers/BancoController.java
+++ src/main/java/com/xantx/financeapp/controllers/BancoController.java
@@ -1,6 +1,7 @@
 package com.xantx.financeapp.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -12,14 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/banco")
+@RequestMapping("api/bancos")
 public class BancoController {
     @Autowired
     private BancoService bancoService;
@@ -47,5 +51,33 @@ public class BancoController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    // TODO: implementacion del resto de metodos crud
+
+    @DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            if (!bancoService.findById(id).isPresent()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            bancoService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Banco> update(@PathVariable Long id, @Valid @RequestBody BancoResource resource) {
+        try {
+            Optional<Banco> banco = bancoService.findById(id);
+            if (!banco.isPresent()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            Banco updatedBanco = banco.get();
+            updatedBanco.setLink(resource.getLink());
+            updatedBanco.setNombre(resource.getNombre());
+            return new ResponseEntity<Banco>(bancoService.update(id, updatedBanco), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
